eb1ec16352237de8fe7d32065ed209559e8c4fdd
Eric Kim <erickim555@gmail.com>
2012-10-29 17:34:06 -0700

    More initial work on the new glue.

--- opencount/glue_new/glue.py
+++ opencount/glue_new/glue.py
@@ -6,24 +6,23 @@ except ImportError as e:
 
 import wx
 from wx.lib.scrolledpanel import ScrolledPanel
-print 'hi'
-sys.path.append('..')
-print 'hi'
 
+sys.path.append('..')
+from tab_wrap import tab_wrap
 from ProjectPanel import ProjectPanel
 from ConfigPanel import ConfigPanel
 from PartitionPanel import PartitionPanel
 from specify_voting_targets.select_targets import SelectTargetsMainPanel
-#from labelcontest.labelcontest import LabelContest
+from labelcontest.labelcontest import LabelContest
 from grouping.define_attributes import DefineAttributesPanel
-#from grouping.select_attributes import SelectAttributesMasterPanel
-#from digits_ui.digits_ui import LabelDigitsPanel
-#from grouping.verify_grouping import GroupingMasterPanel
-#from runtargets.runtargets import RunTargets
-#from quarantine.quarantinepanel import QuarantinePanel
-#from post_processing.postprocess import ResultsPanel
+from grouping.select_attributes import SelectAttributesMasterPanel
+from digits_ui.digits_ui import LabelDigitsPanel
+from grouping.verify_grouping import GroupingMasterPanel
+from runtargets.runtargets import RunTargets
+from threshold.threshold import ThresholdPanel
+from quarantine.quarantinepanel import QuarantinePanel
+from post_processing.postprocess import ResultsPanel
 
-print 'hi' 
 class MainFrame(wx.Frame):
     def __init__(self, parent, *args, **kwargs):
         wx.Frame.__init__(self, parent, *args, **kwargs)
@@ -39,16 +38,16 @@ class MainFrame(wx.Frame):
         self.panel_config = ConfigPanel(self.notebook)
         self.panel_partition = PartitionPanel(self.notebook)
         self.panel_seltargets = SelectTargetsMainPanel(self.notebook)
-        self.panel_define_attrs = DefineAttributesPanel(notebook)
+        self.panel_define_attrs = DefineAttributesPanel(self.notebook)
         self.panel_define_attrs.unsubscribe_pubsubs()
-        self.panel_label_attrs = SelectAttributesMasterPanel(notebook)
-        self.panel_label_digitattrs = tab_wrap(LabelDigitsPanel)(notebook)
-        self.panel_correct_grouping = GroupingMasterPanel(notebook)
-        self.panel_label_contests = tab_wrap(LabelContest)(notebook)
-        self.panel_run = RunTargets(notebook)
-        self.panel_set_threshold = tab_wrap(ThresholdPanel)(notebook)
-        self.panel_quarantine = QuarantinePanel(notebook)
-        self.panel_process = ResultsPanel(notebook)
+        self.panel_label_attrs = SelectAttributesMasterPanel(self.notebook)
+        self.panel_label_digitattrs = tab_wrap(LabelDigitsPanel)(self.notebook)
+        self.panel_correct_grouping = GroupingMasterPanel(self.notebook)
+        self.panel_label_contests = tab_wrap(LabelContest)(self.notebook)
+        self.panel_run = RunTargets(self.notebook)
+        self.panel_set_threshold = tab_wrap(ThresholdPanel)(self.notebook)
+        self.panel_quarantine = QuarantinePanel(self.notebook)
+        self.panel_process = ResultsPanel(self.notebook)
         self.pages = [(self.panel_projects, "Projects"),
                       (self.panel_config, "Import Files"), 
                       (self.panel_partition, "Partition ballots"),
@@ -63,10 +62,8 @@ class MainFrame(wx.Frame):
                       (self.panel_quarantine, "Process Quarantine"),
                       (self.panel_process, "Results")]
         for panel, text in self.pages[1:]:
-            notebook.AddPage(panel, text)
+            self.notebook.AddPage(panel, text)
         
-print 'hi'
-
 def main():
     app = wx.App(False)
     f = MainFrame(None)
@@ -75,7 +72,4 @@ def main():
     app.MainLoop()
 
 if __name__ == '__main__':
-    print 'meow'
     main()
-
-print 'hi'
